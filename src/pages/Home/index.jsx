b5e65ad11145de19137497d8d6eb8ce9b397b758
Vinicius Maclters <vinicius.maclters@hotmail.com>
2023-03-15 16:27:40 -0300

    gitignore

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react"
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 export function Home() {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    
+  }
+
+
   return (
     <div>
       Home
